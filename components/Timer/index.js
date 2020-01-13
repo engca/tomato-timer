17f702d5320f140906732603760d79e20f3b8697
engca <enca1988@naver.com>
2020-01-13 17:06:01 +0900

    #12 Connecting the Components to Actions

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,4 +1,6 @@
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators as tomatoAction } from "../../reducer";
 import Timer from "./presenter";
 
 function mapStateToProps(state) {
@@ -10,4 +12,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Timer);
+function mapDispatchToProps(dispatch) {
+  return {
+    startTimer: bindActionCreators(tomatoAction.startTimer, dispatch),
+    restartTimer: bindActionCreators(tomatoAction.restartTimer, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
